522cabd491a8fa09f76ee6886dd9e2a3eebe0569
Richard Parse <richard.parse@hotmail.com>
2019-09-02 11:35:10 +0200

    Default endpoint implementation.

--- com.bankaccount.speedledger/src/main/java/service/AccountService.java
+++ com.bankaccount.speedledger/src/main/java/service/AccountService.java
@@ -2,6 +2,8 @@ package service;
 
 import com.bankaccount.com.bankaccount.speedledger.BankAccount;
 import com.mongodb.MongoClient;
+
+import java.math.BigDecimal;
 import java.util.List;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
@@ -17,23 +19,54 @@ public class AccountService extends BankAccount {
     }
 
     public List<BankAccount> getAllAccounts() {
-       List<BankAccount> list = datastore.find(BankAccount.class).asList();
+        List<BankAccount> list = datastore.find(BankAccount.class).asList();
         if (list != null) {
             return list;
         }
         return null;
     }
-    
+
     public List<BankAccount> getDefaultAccounts() {
         List<BankAccount> list = datastore.find(BankAccount.class).asList();
-        if (list != null) {
-            if (synthetic == false) {
-                if (balance >= 0) {
+        this.getBankAccount(list);
+        return list;
+
+    }
+
+    public BankAccount getBankAccount(List<BankAccount> bankAccounts) {
+        int counter = 0;
+        BankAccount highestBalanceBankAccount = null;
+        // checks account balance
+        for (BankAccount bankAccount : bankAccounts) {
+            // If there is only one bank account, the id of the account will be returned.
+            // Synthetic accounts can never be chosen as
+            // default accounts.
+            if (bankAccounts.size() == 1 && !bankAccount.isSynthetic()
+                    && bankAccount.getBalance().compareTo(BigDecimal.ZERO) > 0) {
+
+                return bankAccount;
+            }
             
-                return list;
-            }}
-            return null;
+            if (counter == 0) {
+                highestBalanceBankAccount = bankAccount;
+
+            }
+            
+            if (counter != 0 && highestBalanceBankAccount.getBalance().compareTo(bankAccount.getBalance()) == 1) {
+                
+            } else {
+                // If it executes this statement means that we have a creater balance
+                highestBalanceBankAccount = bankAccount;
+
+            }
+
+            counter++;
+
         }
-        return null;
+
+        BigDecimal two = new BigDecimal("2");
+        // half of the highest value
+        BigDecimal metadeValorMaior = highestBalanceBankAccount.getBalance().divide(two);
+        return highestBalanceBankAccount;
     }
 }
