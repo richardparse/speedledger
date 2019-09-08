66f38d36b5d79ad97dc2b533c1caf680e0b6c749
Richard Parse <richard.parse@hotmail.com>
2019-09-08 13:19:08 +0200

    Latest changes in the default endpoint implementation.

---
+++ com.bankaccount.speedledger/src/test/java/com/bankaccount/com/bankaccount/speedledger/PostAddAccountTest.java
@@ -0,0 +1,13 @@
+package com.bankaccount.com.bankaccount.speedledger;
+
+import org.junit.jupiter.api.Test;
+
+
+public class PostAddAccountTest {
+
+    
+    
+    
+    @Test
+    public void 
+}
